ab69c2ed2b676c16eca31e06e46065c9fb9ba206
Ricky Brown <ricky@brilliantlabs.co>
2017-03-27 02:00:09 -0700

    initial commit

---
+++ controllers.js
@@ -0,0 +1,43 @@
+'use strict';
+const WiFiControl = require('wifi-control');
+const wifi    = require('./lib/wifi');
+const express = require('express');
+const router  = express.Router();
+
+router.get('/', function(req, res) {
+  res.send('nonbox server')
+})
+
+// scan for wireless networks
+router.get('/scan', function(req, res) {
+  wifi.scan().then(function(resp){
+    res.status(200).send(resp)
+  }).catch(function(err){
+    res.status(500).send(err)
+  });
+});
+// connect to a network
+router.post('/connect', function(req, res) {
+  console.log(req.body.ap)
+  wifi.connect(req.body.ap).then(function(resp){
+    res.status(200).send(resp)
+  }).catch(function(err){
+    res.status(500).send(err)
+  });
+});
+// status
+router.get('/status', function(req, res) {
+  wifi.status().then(function(resp){
+    res.status(200).send(resp)
+  }).catch(function(err){console.log(err)});
+});
+// reset network interface
+router.delete('/reset', function(req, res) {
+  wifi.reset().then(function(resp){
+    res.status(200).send(resp)
+  }).catch(function(err){
+    res.status(500).send(err)
+  });
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+
+// Parse body as JSON
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/', require('./controllers'));
+
+// Serve API
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+  console.log('nonbox server listening on port ' + server.address().port);
+});
