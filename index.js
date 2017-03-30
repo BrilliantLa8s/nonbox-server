a701b4dd4e27dfd8ac74754aed5b144fc72b9c66
Ricky Brown <ricky@brilliantlabs.co>
2017-03-30 04:31:58 -0700

    add bleno / bluetooth lib

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/', require('./controllers'));
 
+require('./lib/bluetooth');
+
 // Serve API
 app.set('port', process.env.PORT || 3000);
 
